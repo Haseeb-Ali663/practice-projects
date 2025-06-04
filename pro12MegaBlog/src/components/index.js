885e71853d297c3ab528a6f2571bb10fde95a5aa
Haseeb-Ali663 <haydenaliamjad@gmail.com>
2025-06-04 18:47:35 +0500

    today updates(4-6-2025)

---
+++ pro12MegaBlog/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Container from "../components/container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+
+export { Header, Footer, Container, Logo, LogoutBtn };
