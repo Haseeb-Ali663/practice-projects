28d46972f5db633f55d09da3e9fa699d11ba319d
Haseeb-Ali663 <haydenaliamjad@gmail.com>
2025-06-20 17:17:31 +0500

    post form build

--- pro12MegaBlog/src/components/index.js
+++ pro12MegaBlog/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "../components/Footer/Footer";
 import Container from "../components/container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, RTE };
