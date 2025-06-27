822892f6df96a3510c26e8fae553e9f2de77aad2
Haseeb-Ali663 <haydenaliamjad@gmail.com>
2025-06-27 19:01:28 +0500

    today updates ( 27 - 06 - 2025 )

--- pro12MegaBlog/src/main.jsx
+++ pro12MegaBlog/src/main.jsx
@@ -1,9 +1,32 @@
-import { StrictMode } from "react";
+import { Children, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import Home from "./pages/Home.jsx";
+import { AuthLayout, Login } from "./components/index.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    Children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
